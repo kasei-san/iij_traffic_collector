b3fc32264d71e6c673d481444cbbf9e7b3dd4b52
kasei_san <kasei.san@gmail.com>
2016-02-12 11:53:04 +0900

    Add main.rb

---
+++ main.rb
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'mechanize'
+require 'json'
+
+agent = Mechanize.new
+agent.get('https://www.iijmio.jp/auth/login.jsp') do |page|
+  page.form_with(action: '/j_security_check') do |form|
+    form['j_username'] = ENV['IIJ_ID']
+    form['j_password'] = ENV['IIJ_PASS']
+    form.click_button
+  end
+end
+
+now_month = Time.now.month
+usage = 0
+
+agent.get('https://www.iijmio.jp/service/setup/hdd/viewdata/execute.do') do |page|
+  page.form_with(name: 'lteViewDataForm').click_button.tap do |response|
+    response.search('table.base2 tr')[3..-1].each do |tr|
+      date, datasize = tr.search('td')[0..1].map(&:inner_text).map(&:strip)
+      date = Time.parse(date.split(/[年月日]/).join('/'))
+      datasize = datasize.to_i
+      usage += datasize if date.month == now_month
+    end
+  end
+end
+
+uri = URI.parse(ENV['MACKEREL_URI'])
+Net::HTTP.new(uri.host, uri.port).tap do |http|
+  http.use_ssl = true
+  Net::HTTP::Post.new(
+    uri.request_uri,
+    'Content-Type' =>'application/json',
+    'X-Api-Key' => ENV['MACKEREL_API_KEY']
+  ).tap do |request|
+    request.body = [{
+      name:  'transfer.usage',
+      time:  Time.now.to_i,
+      value: usage * 1000 * 1000
+    }].to_json
+    response = http.request(request)
+    p response
+    p response.body
+  end
+end
